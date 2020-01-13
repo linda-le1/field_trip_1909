8b9652d3deba8931dc0120741a9fe41920322e0d
Linda Le <yueling1222@gmail.com>
2020-01-13 11:13:42 -0700

    Fix flight show view with all passing rspec tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,10 @@ southwest = Airline.create(name: "Southwest")
 southwest_1 = southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
 southwest_2 = southwest.flights.create(number: "SW2", date: "12/08/19", time: "0900", departure_city: "Baltimore", arrival_city: "Oakland")
 southwest_3 = southwest.flights.create(number: "SW3", date: "10/10/20", time: "1300", departure_city: "Denver", arrival_city: "New York City")
+
 linda = Passenger.create(name: "Linda", age: 28)
+milly = Passenger.create(name: "Milly", age: 3)
+
 southwest_1.passengers << linda
 southwest_2.passengers << linda
+southwest_1.passengers << milly
