8733b1d76e79eac4399656f2e4815ce7477c1a58
Linda Le <yueling1222@gmail.com>
2020-01-13 10:32:35 -0700

    Add test for user story 3

--- spec/features/passengers/show_spec.rb
+++ spec/features/passengers/show_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Passengers Show Page" do
-
-  before :each do
-    @southwest = Airline.create(name: "Southwest")
-
-    @southwest_1 = @southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
-    @southwest_2 = @southwest.flights.create(number: "SW2", date: "12/08/19", time: "0900", departure_city: "Baltimore", arrival_city: "Oakland")
-
-    @linda = Passenger.create(name: "Linda", age: 28)
-    @dave = Passenger.create(name: "Dave", age:32)
-
-    @southwest_1.passengers << @linda
-    @southwest_2.passengers << @linda
-  end
-
   describe "When I visit the passengers show page" do
+
+    before :each do
+      @southwest = Airline.create(name: "Southwest")
+
+      @southwest_1 = @southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
+      @southwest_2 = @southwest.flights.create(number: "SW2", date: "12/08/19", time: "0900", departure_city: "Baltimore", arrival_city: "Oakland")
+      @southwest_3 = @southwest.flights.create(number: "SW3", date: "10/10/20", time: "1300", departure_city: "Denver", arrival_city: "New York City")
+
+      @linda = Passenger.create(name: "Linda", age: 28)
+      @dave = Passenger.create(name: "Dave", age:32)
+
+      @southwest_1.passengers << @linda
+      @southwest_2.passengers << @linda
+    end
+
     it 'I can see the name of and flights for that passenger' do
 
       visit "/passengers/#{@linda.id}"
@@ -24,11 +25,11 @@ RSpec.describe "Passengers Show Page" do
       expect(page).to_not have_content("Flights for #{@dave.name}")
 
       within "#flight-#{@southwest_1.id}" do
-        expect(page).to have_content("Flight Number: #{@southwest_1.id}")
+        expect(page).to have_content("Flight Number: #{@southwest_1.number}")
       end
 
       within "#flight-#{@southwest_2.id}" do
-        expect(page).to have_content("Flight Number: #{@southwest_2.id}")
+        expect(page).to have_content("Flight Number: #{@southwest_2.number}")
       end
 
     end
@@ -38,11 +39,23 @@ RSpec.describe "Passengers Show Page" do
       visit "passengers/#{@linda.id}"
 
       within "#flight-#{@southwest_1.id}" do
-        expect(page).to have_link("#{@southwest_1.id}")
-        click_on("#{@southwest_1.id}")
+        expect(page).to have_link("#{@southwest_1.number}")
+        click_on("#{@southwest_1.number}")
         expect(current_path).to eq "/flights/#{@southwest_1.id}"
       end
 
     end
+
+    it 'I can add a flight to the passenger' do
+
+      visit "passengers/#{@linda.id}"
+
+      fill_in 'number',      with: 'SW3'
+      click_button 'Add Flight'
+
+      expect(current_path).to eql "passengers/#{@linda.id}"
+      expect(page).to have_content("Flight Number: #{@southwest_3.number}")
+
+    end
   end
-end
+end 
