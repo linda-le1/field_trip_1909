59f7bc1fe24edc378f1515d26bdb943cacb8ea77
Linda Le <yueling1222@gmail.com>
2020-01-13 11:20:05 -0700

    Add additional tests for numericality in passengers age with all passing tests

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe Passenger, type: :model do
         it {should validate_presence_of :name}
         it {should validate_presence_of :age}
         it {should validate_numericality_of :age}
-
+        it {should_not allow_value("Two").for(:age)}
+        it {should allow_value(11).for(:age)}
     end
 
     describe 'relationships' do
